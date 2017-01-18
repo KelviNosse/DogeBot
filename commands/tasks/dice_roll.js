19e317b753879ab69d92c0bb90e1f484c5c9a8df
KelviNosse <kelvinjosse@gmail.com>
2017-01-17 20:25:47 -0600

    Change to spanish message

--- commands/tasks/dice_roll.js
+++ commands/tasks/dice_roll.js
@@ -12,7 +12,7 @@ class DiceRoll extends commando.Command {
 
   async run(message, args){
     var roll = Math.floor(Math.random() * 6) + 1;
-    message.reply('You rolled a '+ roll)
+    message.reply('Tiraste un '+ roll)
   }
 }
 
