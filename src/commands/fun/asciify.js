56233ee5008bbd9254f52f83ec4aa32bc8229fc7
KelviNosse <kelvinjosse@gmail.com>
2017-03-15 02:48:32 -0600

    :rocket: Implementing asciify command with popsicle http request
    Signed-off-by: KelviNosse <kelvinjosse@gmail.com>

---
+++ src/commands/fun/asciify.js
@@ -0,0 +1,39 @@
+const {
+    Command
+} = require('discord.js-commando')
+const popsicle = require('popsicle')
+const emoji_finder = require('../../utils/emo')
+module.exports = class Asciify extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'asciify',
+            group: 'fun',
+            memberName: 'asciify',
+            description: 'Convierte texto a formato ASCII',
+            examples: ['asciify <texto>']
+        })
+        this.site = `http://artii.herokuapp.com/`
+    }
+    get_random_font() {
+        return this.font_list[Math.floor(Math.random() * this.font_list.length)]
+    }
+    async asciify_it(message) {
+        try {
+            let response = await popsicle.get(`${this.site}make?text=${message}`)
+            console.log(response)
+            if (response.body) return {
+                status: response.status,
+                msg: response.body
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+    async run(message, args) {
+        if (!args) return message.say(`Ups! Uso invalido! Ejemplo: asciify "Pony Rosado"`)
+            .then(msg => msg.delete(3000))
+        let ascii = await this.asciify_it(args)
+        if(ascii.status !== 200) return message.say(`Lo siento no se permiten emojis ${emoji_finder(this.client.emojis, 'dogesad')}`)
+        else return message.say('\`\`\`' + ascii.msg + '\`\`\`')
+    }
+}
