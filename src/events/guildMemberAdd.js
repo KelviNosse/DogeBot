7190b91b575b2bbe3bff9c7253c5b4bce1096bd7
KelviNosse <kelvinjosse@gmail.com>
2017-03-15 03:14:25 -0600

    :construction: Wow, very welcome
    Signed-off-by: KelviNosse <kelvinjosse@gmail.com>

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 const emoji_finder = require('../utils/emo')
 exports.run = (client, member) => {
     let guild = member.guild
-    guild.defaultChannel.sendMessage(`Wow ${emoji_finder(client.emojis, 'ohmydoge')} Bienvenido ${member.user} a __**KJCHINTS SERVER**__`)
+    guild.defaultChannel.sendMessage(`Wow ${emoji_finder(client.emojis, 'ohmydoge')} __**Bienvenido seas**__ ${member.user} **:^)**`)
 }
