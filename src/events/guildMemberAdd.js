54edc969fd1b7d410c7e4415cf75b80731528ea1
KelviNosse <kelvinjosse@gmail.com>
2017-03-19 03:24:41 -0600

    :ambulance: Adding role, on new member
    Signed-off-by: KelviNosse <kelvinjosse@gmail.com>

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -2,4 +2,6 @@ const emoji_finder = require('../utils/emo')
 exports.run = (client, member) => {
     let guild = member.guild
     guild.defaultChannel.sendMessage(`Wow ${emoji_finder(client.emojis, 'ohmydoge')} __**Bienvenido seas**__ ${member.user} **:^)**`)
+    //adding default role
+    member.addRole('291726942506319872').catch(console.error)
 }
